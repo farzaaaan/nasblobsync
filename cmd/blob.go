3ac59cbdb520d57abbd0d1ec5b640e52dad0047a
farzan <8821918+farzaaaan@users.noreply.github.com>
2024-02-10 16:02:14 -0500

    changing blob go

--- cmd/blob.go
+++ cmd/blob.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"net/url"
 	"os"
-	"sort"
 	"strings"
+	"sync"
 
 	"github.com/Azure/azure-storage-blob-go/azblob"
 	"github.com/farzaaaan/nasblobsync/cmd/models"
@@ -16,16 +16,17 @@ import (
 )
 
 var (
-	storageAccount          string
-	container               string
-	storageAccountKeyOrConn string
+	storageAccount    string
+	container         string
+	storageAccountKey string
+	initialPrefix     string
 )
 var blobCmd = &cobra.Command{
 	Use:   "blob",
 	Short: "Traverse blob container",
 	Long:  "Traverse blob details and output finding as map[string]{fileSize, lasModified} to blob_details.json",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := GetBlob(storageAccount, container, storageAccountKeyOrConn)
+		err := GetBlob(storageAccount, container, storageAccountKey, initialPrefix)
 		if err != nil {
 			fmt.Println("Error:", err)
 			os.Exit(1)
@@ -38,15 +39,15 @@ func init() {
 
 	blobCmd.Flags().StringVar(&storageAccount, "storage-account", "", "Azure Storage Account Name")
 	blobCmd.Flags().StringVar(&container, "container", "", "Azure Storage Container Name")
-	blobCmd.Flags().StringVar(&storageAccountKeyOrConn, "storage-account-connection-string", "", "Azure Storage Account Connection String or Key")
+	blobCmd.Flags().StringVar(&container, "prefix", "", "prefix")
+	blobCmd.Flags().StringVar(&storageAccountKey, "storage-account-key", "", "Azure Storage Account Connection String or Key")
 	blobCmd.MarkFlagRequired("storage-account")
 	blobCmd.MarkFlagRequired("container")
-	blobCmd.MarkFlagRequired("storage-account-connection-string")
+	blobCmd.MarkFlagRequired("storage-account-key")
 }
 
-func GetBlob(storageAccount, container, storageAccountKeyOrConn string) error {
-
-	credential, err := azblob.NewSharedKeyCredential(storageAccount, storageAccountKeyOrConn)
+func GetBlob(storageAccount, container, accountKey, initialPrefix string) error {
+	credential, err := azblob.NewSharedKeyCredential(storageAccount, accountKey)
 	if err != nil {
 		return err
 	}
@@ -56,55 +57,68 @@ func GetBlob(storageAccount, container, storageAccountKeyOrConn string) error {
 	URL, _ := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net/%s", storageAccount, container))
 	containerURL := azblob.NewContainerURL(*URL, p)
 
-	blobMap := make(map[string]models.FileDetails)
-
 	ctx := context.Background()
-	blobList, err := containerURL.ListBlobsFlatSegment(ctx, azblob.Marker{}, azblob.ListBlobsSegmentOptions{})
-	if err != nil {
-		return err
-	}
 
-	for _, blob := range blobList.Segment.BlobItems {
+	var (
+		wg      sync.WaitGroup
+		mu      sync.Mutex
+		blobMap = make(map[string]models.FileDetails)
+	)
 
-		blobName := strings.TrimPrefix(blob.Name, "/"+container+"/")
-		blobURL := containerURL.NewBlobURL(blobName)
-		props, err := blobURL.GetProperties(ctx, azblob.BlobAccessConditions{}, azblob.ClientProvidedKeyOptions{})
+	var processPrefix func(ctx context.Context, prefix string)
+	processPrefix = func(ctx context.Context, prefix string) {
+		defer wg.Done()
+
+		blobList, err := containerURL.ListBlobsHierarchySegment(ctx, azblob.Marker{}, "/", azblob.ListBlobsSegmentOptions{})
 		if err != nil {
-			return err
+			fmt.Println("Error:", err)
+			return
 		}
 
-		if utils.ShouldIgnoreFile(blobName) {
-			return nil
+		for _, blob := range blobList.Segment.BlobItems {
+			blobName := strings.TrimPrefix(blob.Name, "/"+container+"/")
+			if utils.ShouldIgnoreFile(blobName) {
+				continue
+			}
+
+			props, err := containerURL.NewBlobURL(blob.Name).GetProperties(ctx, azblob.BlobAccessConditions{}, azblob.ClientProvidedKeyOptions{})
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+
+			mu.Lock()
+			blobMap[blob.Name] = models.FileDetails{
+				Size: props.ContentLength(),
+			}
+			mu.Unlock()
 		}
 
-		blobMap[blobName] = models.FileDetails{
-			Size: props.ContentLength(),
+		for _, dir := range blobList.Segment.BlobPrefixes {
+			dirName := strings.TrimSuffix(strings.TrimPrefix(dir.Name, "/"+container+"/"), "/")
+			newPrefix := prefix + "/" + dirName
+			wg.Add(1)
+			go processPrefix(ctx, newPrefix)
 		}
 	}
 
-	var keys []string
-	for key := range blobMap {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
+	wg.Add(1)
+	go processPrefix(ctx, initialPrefix)
 
-	sortedBlobMap := make(map[string]models.FileDetails)
-	for _, key := range keys {
-		sortedBlobMap[key] = blobMap[key]
-	}
+	wg.Wait()
 
-	jsonData, err := json.MarshalIndent(sortedBlobMap, "", "  ")
+	// Marshal and write to blob_details.json file
+	blobDetailsFile, err := os.Create("blob_details.json")
 	if err != nil {
 		return err
 	}
+	defer blobDetailsFile.Close()
 
-	outputFile, err := os.Create("blob_details.json")
+	jsonData, err := json.MarshalIndent(blobMap, "", "  ")
 	if err != nil {
 		return err
 	}
-	defer outputFile.Close()
-
-	_, err = outputFile.Write(jsonData)
+	_, err = blobDetailsFile.Write(jsonData)
 	if err != nil {
 		return err
 	}
